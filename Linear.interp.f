e1c7d6700cf13cb9a7cd3d0420fc69720cf7c105
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-02-28 20:07:11 +0300

    Add files via upload

---
+++ Linear.interp.f
@@ -0,0 +1,20 @@
+        !PROGRAM TO IMPLEMETN LINEAR_INTERP
+        
+        FX = LOG(X)
+
+        PRINT*,"ENTER FIRST POINT:";READ*,X0; Y0 = LOG(X0)
+        PRINT*,Y0;
+        PRINT*,"ENTER SECOND POINT:";READ*,X1; Y1 = LOG(X1)
+        PRINT*,Y1 
+        PRINT*,"ENTER THE X PPOINT TO INTERPOLATE";READ*,XP
+
+        YP = Y0 + ((Y1-Y0)/(X1-X0)) * (XP-X0)
+        
+        FX = LOG(XP);PRINT*,"THE INITIAL F(XP) IS",FX
+        
+3       FORMAT(E12.5)
+        PRINT*,"THE INTORPOLATED  G(XP) IS",YP
+        ERROR = FX-YP;PRINT*,"ERROR IS ",ERROR
+        STOP
+        END
+
